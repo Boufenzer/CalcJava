fefa72184290d95e58c3fe07ea0ac3cb2950f1c1
Boufenzer <mehdi.boufenzer@gmail.com>
2025-01-27 12:01:15 +0100

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Operation calcul = new Operation(20,10);
+
+        System.out.println(calcul.addition());
+        System.out.println(calcul.multiplication());
+        System.out.println(calcul.division());
+        System.out.println(calcul.soustraction());
+        calcul.afficherHistorique();
+
+        Operation calcul2 = new Operation(16.5,3.6);
+        System.out.println(calcul2.addition());
+        System.out.println(calcul2.multiplication());
+        System.out.println(calcul2.division());
+        System.out.println(calcul2.soustraction());
+        calcul2.afficherHistorique();
+
     }
-}
+
+    }
--- src/Operation.java
+++ src/Operation.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Operation {
-}
+    public double nombre1;
+    public double nombre2;
+    private List<String> historique;
+
+    public Operation(double nombre2, double nombre1) {
+        this.nombre1 = nombre2;
+        this.nombre2 = nombre1;
+        this.historique = new ArrayList<>();
+    }
+
+    public double getNombre1() {
+        return nombre1;
+    }
+
+    public double getNombre2() {
+        return nombre2;
+    }
+
+    public double addition() {
+        double resultat = nombre1 + nombre2;
+        historique.add(nombre1+" + "+nombre2+" = "+resultat);
+        return resultat;
+    }
+
+    public double soustraction() {
+        double resultat = nombre1 - nombre2;
+        historique.add(nombre1+" - "+nombre2+" = "+resultat);
+        return resultat;
+    }
+
+    public double division() {
+        double resultat = nombre1 / nombre2;
+        historique.add(nombre1+" / "+nombre2+" = "+resultat);
+        return resultat;
+    }
+
+    public double multiplication() {
+        double resultat = nombre1 * nombre2;
+        historique.add(nombre1+" * "+nombre2+" = "+resultat);
+        return resultat;
+    }
+
+  public void afficherHistorique() {
+
+            System.out.println("historique");
+            for (String histo : historique) {
+                System.out.println(histo);
+            }
+
+
+
+        }
+  }
+
+
+
+
+
